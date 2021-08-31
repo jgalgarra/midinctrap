bdd412c25da0ee067df17689fdd13bb32fbe3618
Javier García-Algarra <jgalgarra@coit.es>
2021-08-31 20:38:20 +0200

    Saving individual plots

--- midtrap_correlations_plot.R
+++ midtrap_correlations_plot.R
@@ -13,10 +13,21 @@ library(reshape2)
 library(ggplot2)
 library(ggrepel)
 library(cowplot)
-library(factoextra)
-library(Rcpp)
+
+library(patchwork)
+
 
 library(corrplot)
+library(grid)
+library(gridExtra)
+
+
+myfun <- function(M){
+  corrplot(M,order="hclust",tl.col="black",tl.cex = 0.8,
+           type = 'upper', diag = FALSE)
+  recordPlot() # record the latest plot
+}
+
 
 criteria <- read.table("config_data/criteria.txt")
 lcriteria <- criteria$V1
@@ -27,91 +38,107 @@ if (!dir.exists(tdir))
 
 start_year <- 1980
 end_year <- 2019
-
-magnitud <- "speed"
-
 countries_msci <- read.csv("input_data/countries_msci.csv")
 group_ASIA <- read.csv(paste0("input_data/group_ASIA.csv"))
 group_LATAM <- read.csv(paste0("input_data/group_LATAM.csv"))
 group_AFRICA <- read.csv(paste0("input_data/group_AFRICA.csv"))
 group_EUROPA <- read.csv(paste0("input_data/group_EUROPA.csv"))
 
-dratio_dt_mmov_breaks <- c(0.01,0.05,0.1,0.25,0.8,1)
-my_breaks <- c(100,1000,10000,25000,50000)
-speed_breaks <-c(-sqrt(0.5),0,sqrt(0.5),1,sqrt(1.5))
-speed_labels <- c(-0.5,0,0.5,1,1.5)
+lmagnitud <- c("ratio","speed")
 
 onlyMSCI <- TRUE
 
-for (criteria in lcriteria)
+#for (criteria in lcriteria)
+for (magnitud in lmagnitud)
 {
-  print(criteria)
-  datos_all <- read.csv(paste0("output_data/all_speeds_",criteria,".csv"))
-  datos_all <- datos_all[(datos_all$Year>=start_year) & (datos_all$Year<=end_year),]
-
-  lp <- unique(datos_all$CountryCode)
-  #lp <- c("CHN","ARG","IRL","ESP","PRT","SGP","ZWE")
-  np <- 1
-  datadist <- data.frame("Country"=c(),"CountryCode"=c(),"Region"=c(),"MSCI Category" = c(),"distX"=c(),"distY"=c())
-  for (k in lp){
-    clean_data <- datos_all[(datos_all$CountryCode == k) & !is.na(datos_all$ratio) & 
-                            !is.na(datos_all$dratio_dt_mmov),]
-    datosx <- datos_all[(datos_all$CountryCode == k) & !is.na(datos_all$dratio_dt_mmov),]$dratio_dt_mmov
-    datosy <- datos_all[(datos_all$Country == k) & !is.na(datos_all$dratio_dt_mmov),]$dratio_dt_mmov
-    datos_MSCI <- countries_msci[countries_msci$ISOCode == k,]
-    if ((length(datosx)==end_year-start_year+1) ){
-      if ((onlyMSCI) & (datos_MSCI$Category != "None")){
-      datadist <- rbind(datadist,data.frame("Country"=k,"CountryCode"= clean_data$CountryCode[1],"Region"=datos_MSCI$Region,
-                                            "MSCI Category"= datos_MSCI$Category, "distX"=mean(clean_data$dratio_dt_mmov),
-                                           "distY"=mean(clean_data$ddratio_dt_mmov_dt_mmov)))
-      if (np == 1)
-        datosseries <- clean_data
+    print(criteria)
+    datos_all <- read.csv(paste0("output_data/all_speeds_",criteria,".csv"))
+    datos_all <- datos_all[(datos_all$Year>=start_year) & (datos_all$Year<=end_year),]
+  
+    lp <- unique(datos_all$CountryCode)
+    #lp <- c("CHN","ARG","IRL","ESP","PRT","SGP","ZWE")
+    np <- 1
+    datadist <- data.frame("Country"=c(),"CountryCode"=c(),"Region"=c(),"MSCI Category" = c(),"distX"=c(),"distY"=c())
+    for (k in lp){
+      clean_data <- datos_all[(datos_all$CountryCode == k) & !is.na(datos_all$ratio) & 
+                              !is.na(datos_all$dratio_dt_mmov),]
+      datosx <- datos_all[(datos_all$CountryCode == k) & !is.na(datos_all$dratio_dt_mmov),]$dratio_dt_mmov
+      datosy <- datos_all[(datos_all$Country == k) & !is.na(datos_all$dratio_dt_mmov),]$dratio_dt_mmov
+      datos_MSCI <- countries_msci[countries_msci$ISOCode == k,]
+      if ((length(datosx)==end_year-start_year+1) ){
+        if ((onlyMSCI) & (datos_MSCI$Category != "None")){
+        datadist <- rbind(datadist,data.frame("Country"=k,"CountryCode"= clean_data$CountryCode[1],"Region"=datos_MSCI$Region,
+                                              "MSCI Category"= datos_MSCI$Category, "distX"=mean(clean_data$dratio_dt_mmov),
+                                             "distY"=mean(clean_data$ddratio_dt_mmov_dt_mmov)))
+        if (np == 1)
+          datosseries <- clean_data
+        else
+          datosseries <- rbind(datosseries,clean_data)
+        np <- np + 1
+        }
+      } 
       else
-        datosseries <- rbind(datosseries,clean_data)
-      np <- np + 1
-      }
-    } 
-    else
-      if ((onlyMSCI) & (datos_MSCI$Category != "None"))
-        print(paste("Excluded",k))
+        if ((onlyMSCI) & (datos_MSCI$Category != "None"))
+          print(paste("Excluded",k))
+    }
+     
+  
+  namesp <- unique(datosseries$CountryCode)
+  ncolumns <- length(namesp)
+  nullcol <- rep(0,end_year-start_year+1)
+  datoscol <- data.frame("1"=nullcol)
+  datosframe <- datoscol
+  for (k in 2:ncolumns){
+    datosframe <- cbind(datosframe,data.frame(k=nullcol))
   }
-   
+  names(datosframe)=namesp
+  
+  for (k in 1:ncolumns){
+    print(names(datosframe)[k])
+    if (magnitud == "ratio"){
+    if (length(datosseries[datosseries$CountryCode==names(datosframe)[k],]$dratio_dt_mmov)==length(datosframe[,k]))
+       datosframe[,k] <- datosseries[datosseries$CountryCode==names(datosframe)[k],]$dratio_dt_mmov
+    }
+    else{
+    if (length(datosseries[datosseries$CountryCode==names(datosframe)[k],]$ratio)==length(datosframe[,k]))
+      datosframe[,k] <- datosseries[datosseries$CountryCode==names(datosframe)[k],]$ratio
+    }
+  }
+  removecols <- c()
+  for (k in 1:ncolumns)
+    if (sum(datosframe[,k] == 0))
+      removecols <- c(removecols,k)
+  if (!is.null((removecols)))
+    datosframe <- datosframe[,-removecols]
+  
+
+  M<-cor(datosframe,use="complete.obs",method="spearman")
+  
+  p <- myfun(M)
+  if (magnitud=="speed")
+   pspeed <- p
+  else
+   pratio <- p
+
+  
+  wplot <- 8.5
+  hplot <- 8
+  nfile <- paste0(tdir,"/CORRELATION_",criteria,"_",magnitud)
+  ppi <- 300
+  png(paste0(nfile,".png"), width=wplot*ppi, height=hplot*ppi, res=ppi)
+  print(p)
+  dev.off()
 }
 
-namesp <- unique(datosseries$CountryCode)
-ncolumns <- length(namesp)
-nullcol <- rep(0,end_year-start_year+1)
-datoscol <- data.frame("1"=nullcol)
-datosframe <- datoscol
-for (k in 2:ncolumns){
-  datosframe <- cbind(datosframe,data.frame(k=nullcol))
-}
-names(datosframe)=namesp
+todo <- plot_grid(
+  pratio,pspeed,labels=c("A","B"),
+  label_size = 16,
+  ncol = 2
+)
+# corrplot(M,order="hclust",tl.col="black",tl.cex = 0.6,
+#          type = 'upper', diag = FALSE)
 
-for (k in 1:ncolumns){
-  print(names(datosframe)[k])
-  if (magnitud == "ratio"){
-  if (length(datosseries[datosseries$CountryCode==names(datosframe)[k],]$dratio_dt_mmov)==length(datosframe[,k]))
-     datosframe[,k] <- datosseries[datosseries$CountryCode==names(datosframe)[k],]$dratio_dt_mmov
-  }
-  else{
-  if (length(datosseries[datosseries$CountryCode==names(datosframe)[k],]$ratio)==length(datosframe[,k]))
-    datosframe[,k] <- datosseries[datosseries$CountryCode==names(datosframe)[k],]$ratio
-  }
-}
-removecols <- c()
-for (k in 1:ncolumns)
-  if (sum(datosframe[,k] == 0))
-    removecols <- c(removecols,k)
-if (!is.null((removecols)))
-  datosframe <- datosframe[,-removecols]
-
-
-M<-cor(datosframe,use="complete.obs",method="spearman")
-corrplot(M,order="hclust",tl.col="black",tl.cex = 0.6,
-         type = 'upper', diag = FALSE)
-
-npaises <- ncol(datosframe)
+#npaises <- ncol(datosframe)
 # MKol <- matrix(nrow = npaises,ncol = npaises)
 # rownames(MKol) <- names(datosframe)
 # colnames(MKol) <- names(datosframe)
